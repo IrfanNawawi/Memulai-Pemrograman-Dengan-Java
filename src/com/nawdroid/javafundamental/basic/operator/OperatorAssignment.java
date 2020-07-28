3c61468033b37c302a0c23843ae1fac9ec5d0402
0446511018 <0446511018@bussan.co.id>
2020-07-28 21:53:56 +0700

    Codelab Operator

---
+++ src/com/nawdroid/javafundamental/basic/operator/OperatorAssignment.java
@@ -0,0 +1,18 @@
+package com.nawdroid.javafundamental.basic.operator;
+
+public class OperatorAssignment {
+    public static void main(String[] args){
+
+        // mData1 merupakan left operand
+        // angka 2 merupakan right operand
+
+        /* operasi ini bisa disebut juga disebut sebagai copy,
+        * karena nilai left operand akan mengkopi nilai right operand
+        */
+
+        int mData1 = 2;
+        int mData2 = 19;
+        System.out.println("Data pada mData1 adalah ->> " + mData1);
+        System.out.println("Data pada mData2 adalah ->> " + mData2);
+    }
+}
