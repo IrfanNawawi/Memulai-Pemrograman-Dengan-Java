f9eb475e23cdb8238b0bd37162727f11a527c069
0446511018 <0446511018@bussan.co.id>
2020-08-02 22:42:42 +0700

    Codelabs Perulangan

---
+++ src/com/nawdroid/javafundamental/basic/perulangan/For.java
@@ -0,0 +1,17 @@
+package com.nawdroid.javafundamental.basic.perulangan;
+
+public class For {
+    public static void main (String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Angka : " + i);
+        }
+
+        /*
+        * Berikut template penulisan 'For'
+        *
+        * for (initialization; termination; increment) {
+            statement(s)
+          }
+        */
+    }
+}
