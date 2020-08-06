c04def36b69d369e8bacdfdc2cbe7e7e9f5bce18
0446511018 <0446511018@bussan.co.id>
2020-08-06 16:23:11 +0700

    Codelabs Function

---
+++ src/com/nawdroid/javafundamental/basic/function/CallFunction.java
@@ -0,0 +1,12 @@
+package com.nawdroid.javafundamental.basic.function;
+
+public class CallFunction {
+    public static void main(String[] args){
+        // Memanggil fungsi
+        cobaFungsi();
+    }
+
+    public static void cobaFungsi(){
+        System.out.println("Ini merupakan bagian dari fungsi");
+    }
+}
---
+++ src/com/nawdroid/javafundamental/basic/function/ReturnFunction.java
@@ -0,0 +1,16 @@
+package com.nawdroid.javafundamental.basic.function;
+
+public class ReturnFunction {
+    public static void main (String[] args) {
+        double p = 7;
+        double l =  6.5;
+
+        double hasil = hitungLuas(p, l); // Memanggil Fungsi
+        System.out.println("Hasilnya adalah = " + hasil);
+    }
+
+    public static double hitungLuas(double panjang, double lebar) {
+        double luas = panjang * lebar;
+        return luas;
+    }
+}
