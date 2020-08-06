4a27606a6be7503c497909dfc003883ba6900303
0446511018 <0446511018@bussan.co.id>
2020-08-06 17:06:09 +0700

    Codelabs Properti Metode

---
+++ src/com/nawdroid/javafundamental/basic/propertimetode/Main.java
@@ -0,0 +1,10 @@
+package com.nawdroid.javafundamental.basic.propertimetode;
+
+public class Main {
+    public static void main(String[] args) {
+        Hewan kucing = new Hewan(2);
+        kucing.makan();
+        kucing.jalan();
+        kucing.lari();
+    }
+}
